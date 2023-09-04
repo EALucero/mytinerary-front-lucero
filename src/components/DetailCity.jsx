a657e812721d306d8aaead9f959b2f21681f950a
EALucero <ea.lucero@outlook.com>
2023-09-04 04:56:56 -0300

    fix2

--- src/components/DetailCity.jsx
+++ src/components/DetailCity.jsx
@@ -24,7 +24,7 @@ export default function DetailCity({ src, alt, text, id, admin }) {
         {
           itineraries.length > 0 ?
             <span onClick={() => setShow(!show)} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer w-[150px] text-center">{show ? ("hide") : ("itineraries")}</span>
-            : <div className="flex flex-col items-center"><img className="w-[10vw]" src="https://i.gifer.com/2wVe.gif" alt="notItinerary" /><p className="text-[18px] text-black">No hay itinerarios aún para esta ciudad</p></div>
+            : <div className={show? "hidden": "flex flex-col items-center"}><img className="w-[10vw]" src="https://i.gifer.com/2wVe.gif" alt="notItinerary" /><p className="text-[18px] text-black">No hay itinerarios aún para esta ciudad</p></div>
         }
         {
           show &&
